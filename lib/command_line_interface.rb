dc89352d254da864ba828dbeabc806140667fa3e
xxDOOMbox <shawwian@gmail.com>
2017-08-29 15:38:18 -0400

    refactored api_communicator.rb and add ascii art

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,29 @@
 def welcome
-  puts "Welcome to the Star Wars Movie Character Finder!"
+  puts "Welcome to the Star Wars API search engine!"
+  puts "*" * 73
+
+ puts"              _________________      ____        __________"
+ puts".       .    /                 |    /    \\   .  |          \\ "
+ puts"    .       /    ______   _____| . /      \\     |    ___    |     .  ."
+ puts"            \\   \\     |   |       /   /\\   \\    |   |___>   |"
+ puts"          .  \\   \\    |   |      /   /__\\   \\.  |         _/   ."
+ puts".     ________>   |   |   | .   /            \\  |   |\\    \\_______  ."
+ puts"     |            /   |   |    /    ______    \\ |   | \\           |"
+ puts"     |___________/    |___|   /____/      \\___ \\|___|  \\__________|    ."
+ puts" .     ____    __  . _____   ____      .  __________   .  _________"
+ puts"      \\    \\  /  \\  /    /  /    \\       |          \\   /         |    ."
+ puts"       \\    \\/    \\/    /  /      \\      |    ___    | /    ______|  ."
+ puts"        \\              /  /   /\\   \\ .   |   |___>   |  \\    \\ "
+ puts"  .      \\            /  /   /__\\   \\    |         _/.   \\    \\ "
+ puts"          \\    /\\    /  /            \\   |   |\\    \\______>    |   ."
+ puts"           \\  /  \\  /  /    ______    \\  |   | \\              /    ."
+ puts".       .   \\/    \\/  /____/      \\____\\ |___|  \\____________/"
+ puts " "
+
+puts "*" * 73
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "Please enter a character to see what films they appeared in:"
   gets.chomp.downcase 
-  # use gets to capture the user's input. This method should return that input, downcased.
 end
